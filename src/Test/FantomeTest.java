9f4767678fe843e04673442331bb5383da17111c
marie-bayle2 <marie.bayle2@etu.univ-lorraine.fr>
2021-01-04 20:07:19 +0100

    ajout des tests

---
+++ src/Test/FantomeTest.java
@@ -0,0 +1,50 @@
+package Test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FantomeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void bouger() {
+    }
+
+    @Test
+    void getx() {
+    }
+
+    @Test
+    void gety() {
+    }
+
+    @Test
+    void getEstActif() {
+    }
+
+    @Test
+    void setx() {
+    }
+
+    @Test
+    void sety() {
+    }
+
+    @Test
+    void setEstActif() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ src/Test/PacmanGameTest.java
@@ -0,0 +1,42 @@
+package Test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PacmanGameTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void evolve() {
+    }
+
+    @Test
+    void isFinished() {
+    }
+
+    @Test
+    void getHero() {
+    }
+
+    @Test
+    void getMonstre() {
+    }
+
+    @Test
+    void getLabyrinthe() {
+    }
+
+    @Test
+    void getFantome() {
+    }
+}
